29867062ffaff293d549e2daa4ebb47fe87127ce
Yanis-Ourir <ourir.yanis@gmail.com>
2024-01-24 13:02:52 +0100

    ajout de mana + sort

--- class/Archer.php
+++ class/Archer.php
@@ -6,7 +6,7 @@ class Archer extends Hero
     public function specialHit(Monster $monster) : int // Mon attaque spéciale si j'ai le mana
     {
         $damage = 30;
-        $this->setMana($this->getMana() - $this->manaCost);
+        $this->setMana($this->getMana() - $this->manaCost); // le héros perd du mana
         $monster->setLifePoint($monster->getLifePoint() - $damage);
         return $damage;
     }
